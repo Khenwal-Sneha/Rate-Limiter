f622c20f3f64182ce2ec03ea7f629b003da485a1
Sneha Khenwal <khenwalsnehas@gmail.com>
2026-05-15 12:53:18 +0530

    Added Upstash Redis support

--- ratelimiter/src/main/java/com/demo/ratelimiter/config/RedisConfig.java
+++ ratelimiter/src/main/java/com/demo/ratelimiter/config/RedisConfig.java
@@ -2,15 +2,45 @@ package com.demo.ratelimiter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
+
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public StringRedisTemplate stringRedisTemplate(
-            org.springframework.data.redis.connection.RedisConnectionFactory factory
-    ) {
-        return new StringRedisTemplate(factory);
+    public LettuceConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration();
+
+        config.setHostName(
+                System.getenv("REDIS_HOST")
+        );
+
+        config.setPort(
+                Integer.parseInt(
+                        System.getenv("REDIS_PORT")
+                )
+        );
+
+        config.setPassword(
+                System.getenv("REDIS_PASSWORD")
+        );
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration
+                        .builder()
+                        .useSsl()
+                        .build();
+
+        return new LettuceConnectionFactory(
+                config,
+                clientConfig
+        );
     }
 }

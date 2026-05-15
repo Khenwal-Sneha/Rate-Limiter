c96e48b78da0fc8ef3be33885ec4d5d4892f0041
Sneha Khenwal <khenwalsnehas@gmail.com>
2026-05-15 13:58:09 +0530

    Connected deployed backend to frontend2

--- dashboard/frontend/src/services/rateLimiterApi.ts
+++ dashboard/frontend/src/services/rateLimiterApi.ts
@@ -4,6 +4,10 @@ import type {
     AlgorithmType
 } from "../types/AlgorithmType"
 
+const API_BASE_URL =
+    import.meta.env
+        .VITE_API_BASE_URL
+
 export const sendRateLimitedRequest =
     async (
         userId: string,
@@ -11,12 +15,16 @@ export const sendRateLimitedRequest =
     ) => {
 
         return axios.get(
-            `/api`,
+
+            `${API_BASE_URL}/api`,
+
             {
                 params: {
                     userId,
                     algorithm
-                }
+                },
+
+                timeout: 60000
             }
         )
     }
--- dashboard/frontend/src/services/userMetricsApi.ts
+++ dashboard/frontend/src/services/userMetricsApi.ts
@@ -1,11 +1,12 @@
 import axios from "axios"
+import { API_BASE_URL } from "../config/apiConfig"
 
 
 export const fetchUserMetrics =
     async (userId: string) => {
 
         return axios.get(
-            `/metrics`,
+            `${API_BASE_URL}/metrics`,
             {
                 params: {
                     userId
